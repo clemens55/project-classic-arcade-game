66323ab003deac0591b1971c8ed0b25ba4488646
Clemens Krametter <clemi.krammi@gmail.com>
2018-09-26 12:13:56 +0200

    Enemies position gets updated

--- js/app.js
+++ js/app.js
@@ -9,12 +9,16 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+//Update enemy position
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+   this.x += this.speed * dt;
+
+    //reset enemy when out of map
+    if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+    
 };
 
 // Draw the enemy on the screen, required method for game
