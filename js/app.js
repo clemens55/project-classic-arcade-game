b469b0725bac20574fa98d608de47db7814f8c2d
Clemens Krametter <clemi.krammi@gmail.com>
2018-09-26 12:09:41 +0200

    Enemy position set and enemies created on map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
